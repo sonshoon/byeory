5b058669d49c19ca9c3a3571d3ef4545b8fbf048
sonshoon <162391329+sonshoon@users.noreply.github.com>
2026-01-17 04:51:26 +0000

    Deploying to gh-pages from @ sonshoon/byeory@c50917e3386799084899950e216d9e44ab3e1c0c 🚀

---
+++ assets/RubyText-DAHDIjkv.js
@@ -0,0 +1,10 @@
+import{t as e}from"./SharedController-BZiy6plN.js";import{Nt as t,W as n,jt as r,z as i}from"./index-Cm2ndhjf.js";import"./Shared-DzneRwI4.js";var a=t(r(),1),o=i(),s=()=>((0,a.useEffect)(()=>(document.body.classList.add(`show-ruby`),()=>{document.body.classList.remove(`show-ruby`)}),[]),(0,o.jsx)(`style`,{children:`
+        body:not(.show-ruby) rt {
+            display: none;
+        }
+        body.show-ruby rt {
+            display: block;
+            color: #ff6b6b;
+            font-size: 0.6em;
+        }
+    `}));const c=({className:t,style:r,gridSize:i})=>(0,o.jsx)(e,{className:t,style:r,label:`루비 문자`,icon:(0,o.jsx)(n,{size:20}),storageKey:`effect-ruby-text`,children:(0,o.jsx)(s,{})});export{c as RubyText};
